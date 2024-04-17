6b4708a3d656ff9d884ca4bc1ec850e2a8318362
Ikigu <ikigu.geo@gmail.com>
2024-04-17 19:28:39 +0300

    feat: add db storage for users

--- models/user.py
+++ models/user.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+
+import os
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
+
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128, nullable=True))
+        last_name = Column(String(128, nullable=True))
     email = ''
     password = ''
     first_name = ''
