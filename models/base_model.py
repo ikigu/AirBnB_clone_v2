42e6f6d9c7817c5f55b663fa58ec1ded25b39042
Ikigu <ikigu.geo@gmail.com>
2024-04-17 23:57:58 +0300

    fix: _

--- models/base_model.py
+++ models/base_model.py
@@ -30,13 +30,17 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+            except KeyError:
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                self.id = str(uuid.uuid4())
+        self.__dict__.update(kwargs)
         self.save()
 
     def __str__(self):
