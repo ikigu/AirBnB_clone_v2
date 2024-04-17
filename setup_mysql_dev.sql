aca57f5e355545646b6385356a641c42309f2062
George Ikigu <ikigu.geo@gmail.com>
2024-04-17 08:53:24 +0300

    fix: use backticks correctly

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the dev user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges to the dev user
-GRANT ALL PRIVILEGES ON `hbnb_dev_db.*` TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 -- Grant SELECT to the deve user in performace schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
