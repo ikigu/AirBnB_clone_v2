869c5665cf9674ea7b09345e9770f6ae7dc84254
Ikigu <ikigu.geo@gmail.com>
2024-04-17 21:06:06 +0300

    fix: use datetime.utcnow() as recommended

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,10 @@ class BaseModel:
 
     if os.environ['HBNB_TYPE_STORAGE'] == 'db':
         id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime, nullable=False, default=datetime.now())
-        updated_at = Column(DateTime, nullable=False, default=datetime.now())
+        created_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
+        updated_at = Column(DateTime, nullable=False,
+                            default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
