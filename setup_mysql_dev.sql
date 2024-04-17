b0811290bc9610c58dfb2def5fd5fb74d6b49a8b
George Ikigu <ikigu.geo@gmail.com>
2024-04-17 08:42:19 +0300

    fix: flush privileges after each grant, add comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,11 @@
 -- Sets up the db for hbnb
-
+-- Creates the development database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
+-- Create the dev user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
+-- Grant all privileges to the dev user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db.*` TO 'hbnb_dev'@'localhost';
-
 FLUSH PRIVILEGES;
-
-GRANT SELECT ON `performance_schema.*` TO 'hbnb_dev'@'localhost';
+-- Grant SELECT to the deve user in performace schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
