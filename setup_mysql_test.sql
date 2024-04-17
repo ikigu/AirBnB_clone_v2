1ad4659225139d6f1acb05647a0df6a1987e45cd
George Ikigu <ikigu.geo@gmail.com>
2024-04-17 08:55:58 +0300

    feat: add script to set up test database

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,14 @@
 -- Sets up the db for hbnb
 -- Creates the test database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
 -- Create the test user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
 -- Grant all privileges to the test user
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
+
 -- Grant SELECT to the teste user in performace schema
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
