14629d0fa1ce106a295c9d958ca698a6f87bf427
Ikigu <ikigu.geo@gmail.com>
2024-04-17 18:36:08 +0300

    fix: change back-populates to cities

--- models/city.py
+++ models/city.py
@@ -19,7 +19,7 @@ class City(BaseModel, Base):
 
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        state = relationship("State", back_populates="children")
+        state = relationship("State", back_populates="cities")
     else:
         state_id = ""
         name = ""
