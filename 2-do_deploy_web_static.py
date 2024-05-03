e860d0f1ec5eca0af3b250041da1d7f900e111b5
Ikigu <ikigu.geo@gmail.com>
2024-05-04 01:48:11 +0300

    iter: use run instead of sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 Compresses web_static files and send to the web server
 """
 
-from fabric.api import put, sudo, env
+from fabric.api import put, run, env
 from os import path
 
 env.hosts = ['100.26.136.10', '54.236.52.202']
@@ -29,11 +29,11 @@ def do_deploy(archive_path):
         return False
 
     # Create folder to uncompress archive to
-    if sudo(f'mkdir -p {releases}/{archive_path[9:-4]}/').failed:
+    if run(f'mkdir -p {releases}/{archive_path[9:-4]}/').failed:
         return False
 
     # Uncompress the archive to the folder created above
-    if sudo(
+    if run(
         f'tar -xzf /tmp/{archive_path[9:]} -C {releases}/{archive_path[9:-4]}/'
     ).failed:
         return False
@@ -41,21 +41,21 @@ def do_deploy(archive_path):
     # Move all the files in uncompressed web_static folder to parent folder
     new_release_folder = f'{releases}/{archive_path[9:-4]}'
     web_static_files = f'{new_release_folder}/web_static/*'
-    if sudo(f'mv {web_static_files} {new_release_folder}').failed:
+    if run(f'mv {web_static_files} {new_release_folder}').failed:
         return False
 
     # Delete the web_static folder (result of uncompressing)
-    if sudo(f'rm -rf {releases}/{archive_path[9:-4]}/web_static').failed:
+    if run(f'rm -rf {releases}/{archive_path[9:-4]}/web_static').failed:
         return False
 
     # Delete the archive from /tmp
-    if sudo(f'rm /tmp/{archive_path[9:]}').failed:
+    if run(f'rm /tmp/{archive_path[9:]}').failed:
         return False
 
     # Delete the symbolic link /data/web_static/current
-    if sudo(f'rm {current}').failed:
+    if run(f'rm {current}').failed:
         return False
 
     # Create new symbolic link, current->
-    if sudo(f'ln -s {releases}/{archive_path[9:-4]} {current}').failed:
+    if run(f'ln -s {releases}/{archive_path[9:-4]} {current}').failed:
         return False
