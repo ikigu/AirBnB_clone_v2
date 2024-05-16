f6b283d69aec845199a008c240b6135e41597cb2
Ikigu <ikigu.geo@gmail.com>
2024-05-16 16:40:35 +0300

    feat: implement hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """Root path"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """hbnb path"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
