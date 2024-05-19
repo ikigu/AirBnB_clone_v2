dc62e95960e92b9b86e5a1dd7023ff5b08288fae
Ikigu <ikigu.geo@gmail.com>
2024-05-19 17:48:34 +0300

    refactor: declare the teardown methode first

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -30,17 +30,17 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(self):
+    """Destroys SQLAlchemy session"""
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Gets all states and passes them to a template"""
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(self):
-    """Destroys SQLAlchemy session"""
-    storage.close()
 
 
 if __name__ == "__main__":
